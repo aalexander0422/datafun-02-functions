34bb0a2cf3ae8b91440b87ae16935e82f0e59f7c
aalexander0422 <122483416+aalexander0422@users.noreply.github.com>
2023-01-22 12:48:42 -0500

    comm

---
+++ pybuddy_oopout.py
@@ -0,0 +1,22 @@
+#Welcome, I'm a new PyBuddy.
+
+#I'm Rex.
+#I'm a Species.DOG with 4 legs.
+#I weigh 10.44 kgs.
+#I am 3 feet tall.
+#I'm available for tutoring.
+#I know:
+  #- Git
+ # - GitHub
+  #- Python
+  #- Markdown
+ # - VS Code
+  #- and stats
+
+
+
+       # You'll need curiousity, the ability to search the web,
+       # and the tenacity and resourcefulness
+       # to solve all kinds of challenges.
+
+       # Let's get started!
